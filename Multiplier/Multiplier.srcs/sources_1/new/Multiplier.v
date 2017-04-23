248a534b1a630b92c73f12d04e76bca954e84b9f
weizhangccnu <296448435@qq.com>
2017-04-24 00:13:01 +0800

    Upload Multiplier.v file

---
+++ Multiplier/Multiplier.srcs/sources_1/new/Multiplier.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/23/2017 09:25:48 PM
+// Design Name: 
+// Module Name: Multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Multiplier(
+input clk,              //input clock
+input a,                //input data a, width = 4bit
+input b,                //input data b, width = 4bit
+//input clk_en,           //clock enable
+//input sclear,           //sychnorous clear
+output out              //multiply result width = 8bit
+    );
+wire clk;
+wire [3:0]a;
+wire [3:0]b;
+wire [7:0]out;
+
+mult_gen_0 mult (
+  .CLK(clk),  // input wire CLK
+  .A(a),      // input wire [3 : 0] A
+  .B(b),      // input wire [3 : 0] B
+  .P(out)      // output wire [7 : 0] P
+);
+endmodule

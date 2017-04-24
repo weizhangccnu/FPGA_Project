aa43c58ace6aeb1fbf0c0d84b9fbc1850dc11d6f
weizhangccnu <296448435@qq.com>
2017-04-24 08:46:03 +0800

    Add clock enable and sychnorous clear signals

--- Multiplier/Multiplier.srcs/sources_1/new/Multiplier.v
+++ Multiplier/Multiplier.srcs/sources_1/new/Multiplier.v
@@ -24,19 +24,23 @@ module Multiplier(
 input clk,              //input clock
 input a,                //input data a, width = 4bit
 input b,                //input data b, width = 4bit
-//input clk_en,           //clock enable
-//input sclear,           //sychnorous clear
+input clk_en,           //clock enable
+input sclear,           //sychnorous clear
 output out              //multiply result width = 8bit
     );
 wire clk;
+wire clk_en;
+wire sclear;
 wire [3:0]a;
 wire [3:0]b;
 wire [7:0]out;
 
 mult_gen_0 mult (
-  .CLK(clk),  // input wire CLK
-  .A(a),      // input wire [3 : 0] A
-  .B(b),      // input wire [3 : 0] B
-  .P(out)      // output wire [7 : 0] P
+  .CLK(clk),        // input wire CLK
+  .A(a),            // input wire [3 : 0] A
+  .B(b),            // input wire [3 : 0] B
+  .CE(clk_en),      // input wire CE
+  .SCLR(sclear),    // input wire SCLR
+  .P(out)           // output wire [7 : 0] P
 );
 endmodule

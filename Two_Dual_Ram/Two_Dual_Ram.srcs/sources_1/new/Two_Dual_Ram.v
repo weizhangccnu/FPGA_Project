baf43e999580be0b809ac979d25e2c1d83ad63ca
weizhangccnu <296448435@qq.com>
2017-04-26 15:40:31 +0800

    Correct use readmemh instruction

--- Two_Dual_Ram/Two_Dual_Ram.srcs/sources_1/new/Two_Dual_Ram.v
+++ Two_Dual_Ram/Two_Dual_Ram.srcs/sources_1/new/Two_Dual_Ram.v
@@ -19,24 +19,31 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module Two_Dual_Ram(
-input rst,          //system reset
-input [7:0]ain,     //input address
-input [7:0]aout,    //output address
-input [7:0]din,     //input data
-input wr_clk,       //write clock
-input wr_en,        //write enable, active is high
-input rd_clk,       //read clock
-input rd_en,        //read enable, active is high
-output reg [7:0]dout    //read output data
+input rst,                  //system reset
+input [7:0]ain,             //input address
+input [7:0]aout,            //output address
+input [7:0]din,             //input data
+input wr_clk,               //write clock
+input wr_en,                //write enable, active is high
+input rd_clk,               //read clock
+input rd_en,                //read enable, active is high
+output reg [15:0]dout       //read output data
     );
 
 reg [7:0]memory[0:255];     //define a memory, width = 8bit, depth = 256
+reg [15:0]H_data;
+reg [15:0]L_data;
 
 initial begin
-$readmemh("./Init_Memory.dat", memory);       //Initial memory with "Init_Memory.dat"
+$readmemh("/home/wz/Desktop/FPGA_Project/Two_Dual_Ram/Init_Memory.txt", memory);       //Initial memory with "Init_Memory.dat"
 end
+
+//integer i;                  //initial reg array
+//initial begin
+//for(i=0;i<256;i=i+1)
+//    memory[i] = 8'h0;
+//end
 
 always @(posedge wr_clk)    //write operation
 begin
@@ -45,6 +52,8 @@ begin
         begin
             memory[ain] <= din;
         end
+    else
+        $readmemh("/home/wz/Desktop/FPGA_Project/Two_Dual_Ram/Init_Memory.txt", memory); 
 end
 
 always @(posedge rd_clk)    //read operation
@@ -52,7 +61,9 @@ begin
     if(!rst)
         if(rd_en)
         begin
-            dout <= memory[aout];
+            H_data <= memory[aout];
+            L_data <= memory[aout+1];
+            dout <= H_data<<8 | L_data;
         end
 end
 endmodule

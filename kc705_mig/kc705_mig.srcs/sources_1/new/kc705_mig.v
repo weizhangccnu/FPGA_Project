46712a92544bc629b0e027bd7456565e14781e47
weizhangccnu <296448435@qq.com>
2018-03-19 20:53:27 +0800

    add gig_eth module to the project

--- kc705_mig/kc705_mig.srcs/sources_1/new/kc705_mig.v
+++ kc705_mig/kc705_mig.srcs/sources_1/new/kc705_mig.v
@@ -18,26 +18,34 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module kc705_mig(
 input SYS_RST,              //system reset
 input SYS_CLK_P,            //system clock 200MHz
 input SYS_CLK_N,
 input SGMIICLK_Q0_P,        //125MHz for GTP/GTH/GTX  for 1G Ethernet interface
-input SGMIICLK_Q0_N,        
-output [7:0]LED8Bit         //clock output
-    );
+input SGMIICLK_Q0_N,
+//----------------------< LED        
+output [7:0]LED8Bit,        //clock output
+//----------------------< DIPSw4Bit
+input  [3:0]DIPSw4Bit,
+//----------------------> Gigbit eth interface (RGMII)
+output PHY_RESET_N,
+output [3:0]RGMII_TXD,
+output RGMII_TX_CTL,
+output RGMII_TXC,
+input  [3:0]RGMII_RXD,
+input  RGMII_RX_CTL,
+input  RGMII_RXC,
+inout  MDIO,
+output MDC
+);
 //---------------------------------------------------------< signal define
 wire reset;
 wire sys_clk;
 wire clk_25MHz;
 wire clk_50MHz;
 wire clk_100MHz;
-wire clk_125MHz;
 wire clk_200MHz;
-wire clk_sgmii_i;
-wire clk_sgmii;
 //---------------------------------------------------------> signal define
 //---------------------------------------------------------< global_clock_reset
 global_clock_reset global_clock_reset_inst(
@@ -55,6 +63,9 @@ global_clock_reset global_clock_reset_inst(
   );
 //---------------------------------------------------------> global_clock_reset
 //---------------------------------------------------------< generate sgmii_i clock
+wire clk_sgmii_i;
+wire clk_sgmii;
+wire clk_125MHz;
 IBUFDS_GTE2 #(
    .CLKCM_CFG("TRUE"),          // Refer to Transceiver User Guide
    .CLKRCV_TRST("TRUE"),        // Refer to Transceiver User Guide
@@ -80,26 +91,108 @@ dbg_ila dbg_ila_inst(
 	.probe0(counter[3:0]) // input wire [3:0] probe0
 );
 //---------------------------------------------------------> dbg_ila
-reg [23:0]counter;
+//---------------------------------------------------------< vio core
+wire [15:0]probe_in0;
+wire [15:0]probe_out0;
+assign probe_in0 = {12'h000,DIPSw4Bit[3:0]};
+vio_0 vio_0_inst (
+  .clk(clk_25MHz),                // input wire clk
+  .probe_in0(probe_in0),    // input wire [15 : 0] probe_in0
+  .probe_out0(probe_out0)  // output wire [15 : 0] probe_out0
+);
+//---------------------------------------------------------> vio core
+reg [24:0]counter;
+wire select;
+assign select = probe_out0[0];
 always @(posedge clk_125MHz)
 begin
     if(reset)
-        counter <= 24'd0;
+        counter <= 25'd0;
     else
     begin
         counter <= counter + 1'b1; 
-        if(counter == 24'h7fffff)
-            counter <= 24'd0;
+        if(counter == 25'h1ffffff)
+            counter <= 25'd0;
     end
 end
 assign LED8Bit[0] = counter[22];
-assign LED8Bit[1] = counter[22];
-assign LED8Bit[2] = counter[22];
-assign LED8Bit[3] = counter[22];
-assign LED8Bit[4] = counter[22];
-assign LED8Bit[5] = counter[22];
-assign LED8Bit[6] = counter[22];
-assign LED8Bit[7] = counter[22];
+assign LED8Bit[1] = counter[24];
+assign LED8Bit[2] = select?counter[22]:counter[24];
+assign LED8Bit[3] = select?counter[22]:counter[24];
+assign LED8Bit[4] = select?counter[22]:counter[24];
+assign LED8Bit[5] = select?counter[22]:counter[24];
+assign LED8Bit[6] = select?counter[22]:counter[24];
+assign LED8Bit[7] = select?counter[22]:counter[24];
+//---------------------------------------------------------< gig_eth 
+wire [47:0]gig_eth_mac_addr;
+wire [31:0]gig_eth_ipv4_addr;
+wire [31:0]gig_eth_subnet_mask;
+wire [31:0]gig_eth_gateway_ip_addr; 
+wire [7:0]gig_eth_tx_tdata;
+wire gig_eth_tx_tvalid;
+wire gig_eth_tx_tready;
+wire [7:0]gig_eth_rx_tdata;
+wire gig_eth_rx_tvalid;
+wire gig_eth_rx_tready;
+wire gig_eth_tcp_use_fifo;
+wire gig_eth_tx_fifo_wrclk;
+wire [31:0]gig_eth_tx_fifo_q;
+wire gig_eth_tx_fifo_wren;
+wire gig_eth_tx_fifo_full;
+wire gig_eth_rx_fifo_rdclk;
+wire gig_eth_rx_fifo_q;
+wire gig_eth_rx_fifo_rden;
+wire gig_eth_rx_fifo_empty;
+assign  set_ipv4_addr = {28'hc0a8020,DIPSw4Bit[3:0]};
+//assign gpio_high = 2'b11;
+gig_eth gig_eth_inst
+(
+// asynchronous reset
+   .GLBL_RST(reset),
+// clocks
+   .GTX_CLK(clk_125MHz),
+   .REF_CLK(sys_clk), // 200MHz for IODELAY
+// PHY interface
+   .PHY_RESETN(PHY_RESET_N),
+//         -- RGMII Interface
+   .RGMII_TXD(RGMII_TXD),
+   .RGMII_TX_CTL(RGMII_TX_CTL),
+   .RGMII_TXC(RGMII_TXC),
+   .RGMII_RXD(RGMII_RXD),
+   .RGMII_RX_CTL(RGMII_RX_CTL),
+   .RGMII_RXC(RGMII_RXC),
+// MDIO Interface
+   .MDIO(MDIO),
+   .MDC(MDC),
+// TCP
+   .MAC_ADDR(48'h000a3502a758),
+   .IPv4_ADDR(set_ipv4_addr),
+   .IPv6_ADDR(128'h0),
+   .SUBNET_MASK(32'hffffff00),
+   .GATEWAY_IP_ADDR(32'hc0a80201),
+   .TCP_CONNECTION_RESET(1'b0),
+   .TX_TDATA(gig_eth_tx_tdata),
+   .TX_TVALID(gig_eth_tx_tvalid),
+   .TX_TREADY(gig_eth_tx_tready),
+   .RX_TDATA(gig_eth_rx_tdata),
+   .RX_TVALID(gig_eth_rx_tvalid),
+   .RX_TREADY(gig_eth_rx_tready),
+//fifo8to32 and fifo32to8
+   .TCP_USE_FIFO(gig_eth_tcp_use_fifo),
+   .TX_FIFO_WRCLK(gig_eth_tx_fifo_wrclk),
+   .TX_FIFO_Q(gig_eth_tx_fifo_q),
+   .TX_FIFO_WREN(gig_eth_tx_fifo_wren),
+   .TX_FIFO_FULL(gig_eth_tx_fifo_full),
+   .RX_FIFO_RDCLK(gig_eth_rx_fifo_rdclk),
+   .RX_FIFO_Q(gig_eth_rx_fifo_q),
+   .RX_FIFO_RDEN(gig_eth_rx_fifo_rden),
+   .RX_FIFO_EMPTY(gig_eth_rx_fifo_empty)
+);
+//---------------------------------------------------------> gig_eth
+//---------------------------------------------------------< control_interface
+wire control_clk;
+assign control_clk = clk_100MHz;
+//---------------------------------------------------------> control_interface
 //---------------------------------------------------------< mig_7series_0
 //  mig_7series_0 mig_7series_0_inst (
 

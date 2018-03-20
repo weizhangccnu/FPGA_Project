cf2f8fae2315d2ca19ee6aa444ea83128a336e56
weizhangccnu <296448435@qq.com>
2018-03-20 14:50:54 +0800

    upload fifo36x512.xci file

---
+++ kc705_mig/ipcore_dir/fifo36x512/fifo36x512_stub.vhdl
@@ -0,0 +1,38 @@
+-- Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2016.2 (lin64) Build 1577090 Thu Jun  2 16:32:35 MDT 2016
+-- Date        : Tue Mar 20 10:58:24 2018
+-- Host        : localhost.localdomain running 64-bit unknown
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/weizhang/Desktop/FPGA_Project/kc705_mig/ipcore_dir/fifo36x512/fifo36x512_stub.vhdl
+-- Design      : fifo36x512
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7k325tffg900-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fifo36x512 is
+  Port ( 
+    rst : in STD_LOGIC;
+    wr_clk : in STD_LOGIC;
+    rd_clk : in STD_LOGIC;
+    din : in STD_LOGIC_VECTOR ( 35 downto 0 );
+    wr_en : in STD_LOGIC;
+    rd_en : in STD_LOGIC;
+    dout : out STD_LOGIC_VECTOR ( 35 downto 0 );
+    full : out STD_LOGIC;
+    empty : out STD_LOGIC
+  );
+
+end fifo36x512;
+
+architecture stub of fifo36x512 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "rst,wr_clk,rd_clk,din[35:0],wr_en,rd_en,dout[35:0],full,empty";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fifo_generator_v13_1_1,Vivado 2016.2";
+begin
+end;

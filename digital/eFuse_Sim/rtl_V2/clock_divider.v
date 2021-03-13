4bceb575d4694858300e90efea93f181fb045455
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2021-03-13 16:13:46 -0600

    40 MHz clock input divided by 5

--- digital/eFuse_Sim/rtl_V2/clock_divider.v
+++ digital/eFuse_Sim/rtl_V2/clock_divider.v
@@ -14,30 +14,60 @@ module clock_divider(
 	output clk_out					// 2 MHz clock output
 );
 
-reg [3:0] cnt;
-reg int_clk2M;
+reg [2:0] cnt1, cnt2;
+reg clk_p, clk_n;
+wire int_clk8M;
 
 always @(posedge int_clock)
 begin
 	if(!rst)
 	begin
-		cnt <= 4'b0000;
-		int_clk2M <= 1'b0;
+		cnt1 <= 3'b000;
+		clk_p <= 1'b0;
 	end
 	else
 	begin
-		if (cnt == 4'd9)
+		if (cnt1 == 3'd4)
 		begin
-			cnt <= 4'd0;
-			int_clk2M <= !int_clk2M;
+			cnt1 <= 3'd0;
+			clk_p <= clk_p;
 		end 
 		else
 		begin
-			cnt <= cnt + 4'd1;
+			cnt1 <= cnt1 + 3'd1;
+			if (cnt1 == 3'd1 || cnt1 == 3'd3)
+			begin
+				clk_p <= !clk_p;
+			end 
 		end
 	end
 end
 
-assign clk_out = (clk_sel == 1'b1) ? int_clk2M : ext_clock;
+always @(negedge int_clock)
+begin
+	if(!rst)
+	begin
+		cnt2 <= 3'b000;
+		clk_n <= 1'b0;
+	end
+	else
+	begin
+		if (cnt2 == 3'd4)
+		begin
+			cnt2 <= 3'd0;
+			clk_n <= clk_n;
+		end 
+		else
+		begin
+			cnt2 <= cnt2 + 3'd1;
+			if (cnt2 == 3'd1 || cnt2 == 3'd3)
+			begin
+				clk_n <= !clk_n;
+			end 
+		end
+	end
+end
+assign int_clk8M = clk_p | clk_n; 
+assign clk_out = (clk_sel == 1'b1) ? int_clk8M : ext_clock;
 endmodule
 

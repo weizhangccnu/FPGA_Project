b032b60cdc62e1306eddbc1caa710ec07778df49
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2021-03-15 17:50:58 -0500

    start rising edge to trigger start_pulse

--- digital/eFuse_Sim/rtl_V2/efuse_controller.v
+++ digital/eFuse_Sim/rtl_V2/efuse_controller.v
@@ -24,11 +24,43 @@ module efuse_controller(
 );
 
 wire clk_8M;
-wire start_delay;
-wire start_pulse;
+reg [2:0] start_cnt;
+reg enable_cnt1, enable_cnt2;
+reg start_pulse;
+always @(posedge start or negedge rst)
+begin
+	if(!rst)
+	begin
+		enable_cnt1 = 1'b0;
+	end
+	else
+	begin
+		enable_cnt1 = 1'b1;
+	end
+end
 
-assign #500 start_delay = start;
-assign start_pulse = start & !start_delay;
+always @(posedge clk_8M)
+begin
+	if(!rst)
+	begin
+		start_cnt <= 3'b000;
+		start_pulse <= 1'b0;
+		enable_cnt2 = 1'b1;
+	end
+	else if(enable_cnt1 == 1'b1 && enable_cnt2 == 1'b1)
+	begin
+		if(start_cnt != 3'b111)
+		begin
+			start_cnt <= start_cnt + 1'b1;
+			start_pulse <= 1'b1;
+		end
+		else
+		begin
+			enable_cnt2 <= 1'b0;
+			start_pulse <= 1'b0;
+		end
+	end
+end
 
 clock_divider clock_divider_inst(
 .int_clock(int_clk),			// internal 40 MHz clock from PLL
